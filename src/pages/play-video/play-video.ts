ad4c868d0c65b1c687196fc6407d2b29ce8978b5
vaibhav <vaibhav.jain@tesco.com>
2018-08-07 18:24:15 +0530

    Play youtube video

--- src/pages/play-video/play-video.ts
+++ src/pages/play-video/play-video.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-play-video',
   templateUrl: 'play-video.html'
 })
 export class PlayVideoPage {
-
-  constructor(public navCtrl: NavController) {
+  videoUrl: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public sanitizer: DomSanitizer) {
+     this.videoUrl = navParams.get('data');
   }
-  
+
 }
