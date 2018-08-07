29de6f67fabfa281a29dc67e3481d7d12193b64a
ravitejaobelisk <ravi@nineleaps.com>
2018-08-07 17:48:50 +0530

    added delete and details operations for program

--- src/pages/program-details/program-details.ts
+++ src/pages/program-details/program-details.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+export interface Program { id:string, name: string; description: string; image: string; }
+
 
 @Component({
   selector: 'page-program-details',
   templateUrl: 'program-details.html'
 })
 export class ProgramDetailsPage {
+  programImage:string;
+  program: Program;
+  programCollection: AngularFirestoreCollection<Program>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public afDatabase: AngularFirestore) {
+    this.programCollection = afDatabase.collection<Program>('programs');
+    this.programImage = navParams.get("programImage");
+    this.program = navParams.get("program");
+  }
+
+
+  deleteProgram(programId) {
+    console.log(programId);
+    // delete the program
+    this.programCollection.doc(programId).delete();
+
+    // switch back to the previous page
+    this.navCtrl.pop();
   }
   
 }
