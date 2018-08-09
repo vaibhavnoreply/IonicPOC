b50bfc993c1a1c162947f0b0c7d0cb6bed93cec7
vaibhav <vaibhav.jain@tesco.com>
2018-08-09 11:45:02 +0530

    Video issue fxi

--- src/pages/play-video/play-video.ts
+++ src/pages/play-video/play-video.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { DomSanitizer } from '@angular/platform-browser';
+import { NavController, NavParams, Loading, LoadingController } from 'ionic-angular';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-play-video',
   templateUrl: 'play-video.html'
 })
 export class PlayVideoPage {
-  videoUrl: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public sanitizer: DomSanitizer) {
-     this.videoUrl = navParams.get('data');
-  }
+  //videoUrl: string;
+  video: any = {
+    url: navParams.get('url'),
+    title: navParams.get('title'),
+  };
+
+  trustedVideoUrl: SafeResourceUrl;
+  loading: Loading;
+  videoTitle: string;
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    private domSanitizer: DomSanitizer ) {}
+
+
+    ionViewWillEnter(): void {
+      this.trustedVideoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.video.url);
+      this.videoTitle = this.video.title;
+      this.loading = this.loadingCtrl.create({
+        content: 'Please wait...'
+      });
+
+      this.loading.present();
+    }
+
+    handleIFrameLoadEvent(): void {
+      this.loading.dismiss();
+    }
 
 }
